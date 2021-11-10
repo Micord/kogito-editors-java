ec14c0daee636e91f018a568ca63845db30b018a
EvgenyMalkov <malk.2011@yandex.ru>
2021-11-10 16:17:42 +0300

    WEBBPMNEXT-7873: JSType class

--- lienzo-webapp/src/main/java/org/kie/lienzo/client/LienzoGWT2Examples.java
+++ lienzo-webapp/src/main/java/org/kie/lienzo/client/LienzoGWT2Examples.java
@@ -1,13 +1,11 @@
 package org.kie.lienzo.client;
 
 import com.google.gwt.core.client.EntryPoint;
-import org.timepedia.exporter.client.ExporterUtil;
 
 public class LienzoGWT2Examples extends BaseLienzoExamples implements EntryPoint {
 
     @Override
     public void onModuleLoad() {
         doLoad();
-        ExporterUtil.exportAll();
     }
 }
