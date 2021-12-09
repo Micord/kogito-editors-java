53e60149f210f831531de9f5c0e9d9e476e23362
EvgenyMalkov <malk.2011@yandex.ru>
2021-12-09 17:06:06 +0300

    WEBBPMNEXT-7873: change max size for role's combobox

--- kie-wb-common-stunner/kie-wb-common-stunner-sets/kie-wb-common-stunner-bpmn/kie-wb-common-stunner-bpmn-client/src/main/java/org/kie/workbench/common/stunner/bpmn/client/forms/fields/assigneeEditor/widget/AssigneeLiveSearchEntryCreationEditor.java
+++ kie-wb-common-stunner/kie-wb-common-stunner-sets/kie-wb-common-stunner-bpmn/kie-wb-common-stunner-bpmn-client/src/main/java/org/kie/workbench/common/stunner/bpmn/client/forms/fields/assigneeEditor/widget/AssigneeLiveSearchEntryCreationEditor.java
@@ -59,7 +59,7 @@ public class AssigneeLiveSearchEntryCreationEditor implements InlineCreationEdit
     }
 
     public static native String getUrl()  /*-{
-        return "A&B&C&D&E&F&G&H&I&G&K&L&M&N&O&P&Q&R&S&T&U&V&W&X&Y&Z"
+        return roles.split("&");
     }-*/;
 
     @Override
