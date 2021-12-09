cc26733a05b399a2dfd547c9b38a1d8f2763835d
EvgenyMalkov <malk.2011@yandex.ru>
2021-12-09 15:09:02 +0300

    WEBBPMNEXT-7873: something roles for test

--- kie-wb-common-stunner/kie-wb-common-stunner-sets/kie-wb-common-stunner-bpmn/kie-wb-common-stunner-bpmn-client/src/main/java/org/kie/workbench/common/stunner/bpmn/client/forms/fields/assigneeEditor/widget/AssigneeLiveSearchEntryCreationEditor.java
+++ kie-wb-common-stunner/kie-wb-common-stunner-sets/kie-wb-common-stunner-bpmn/kie-wb-common-stunner-bpmn-client/src/main/java/org/kie/workbench/common/stunner/bpmn/client/forms/fields/assigneeEditor/widget/AssigneeLiveSearchEntryCreationEditor.java
@@ -59,7 +59,7 @@ public class AssigneeLiveSearchEntryCreationEditor implements InlineCreationEdit
     }
 
     public static native String getUrl()  /*-{
-        return parent.parent.projectRoles.projectRoles;
+        return "A&B&C&D&E&F&G&H&I&G&K&L&M&N&O&P&Q&R&S&T&U&V&W&X&Y&Z"
     }-*/;
 
     @Override
