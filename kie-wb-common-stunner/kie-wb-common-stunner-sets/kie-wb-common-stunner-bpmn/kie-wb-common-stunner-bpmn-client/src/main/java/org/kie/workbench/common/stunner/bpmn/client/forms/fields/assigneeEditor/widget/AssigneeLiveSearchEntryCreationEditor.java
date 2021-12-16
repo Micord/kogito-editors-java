e153729556253f1351d77e9fdd5570123715d095
EvgenyMalkov <malk.2011@yandex.ru>
2021-12-16 11:07:13 +0300

    WEBBPMNEXT-7873: without okCommand

--- kie-wb-common-stunner/kie-wb-common-stunner-sets/kie-wb-common-stunner-bpmn/kie-wb-common-stunner-bpmn-client/src/main/java/org/kie/workbench/common/stunner/bpmn/client/forms/fields/assigneeEditor/widget/AssigneeLiveSearchEntryCreationEditor.java
+++ kie-wb-common-stunner/kie-wb-common-stunner-sets/kie-wb-common-stunner-bpmn/kie-wb-common-stunner-bpmn-client/src/main/java/org/kie/workbench/common/stunner/bpmn/client/forms/fields/assigneeEditor/widget/AssigneeLiveSearchEntryCreationEditor.java
@@ -67,7 +67,7 @@ public class AssigneeLiveSearchEntryCreationEditor implements InlineCreationEdit
         String[] rolesArray = delimiterRoles(roles);
         for(String role : rolesArray) {
             if (isValid(role)) {
-                this.okCommand.execute(new LiveSearchEntry<>(role, role));
+                this.customEntryCommand.execute(role);
             }
         }
     }
